bfefe54cfe0c2999b6b69c3a276d4beacf7f7615
gromoff97 <gromoff97@mail.ru>
2019-05-15 19:23:35 +0300

    regular expression for validating email address was created

--- src/main/java/home/grom/urljournaling/JournalComparisonNotifier.java
+++ src/main/java/home/grom/urljournaling/JournalComparisonNotifier.java
@@ -1,5 +1,7 @@
 package home.grom.urljournaling;
 
 public final class JournalComparisonNotifier {
+    private final static String emailRegex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
     private JournalComparisonNotifier(){}
 }
