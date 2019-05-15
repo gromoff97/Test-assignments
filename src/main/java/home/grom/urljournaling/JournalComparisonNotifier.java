a0ed5c732dcf1e9da5d78f03177497cb66ba684f
gromoff97 <gromoff97@mail.ru>
2019-05-15 19:48:27 +0300

    method for sending email with journal comparisons was created

--- src/main/java/home/grom/urljournaling/JournalComparisonNotifier.java
+++ src/main/java/home/grom/urljournaling/JournalComparisonNotifier.java
@@ -1,7 +1,15 @@
 package home.grom.urljournaling;
 
+import org.simplejavamail.mailer.Mailer;
+
 public final class JournalComparisonNotifier {
     private final static String emailRegex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
     private JournalComparisonNotifier(){}
+
+    public void sendComparisonResults(final URLHTMLVisitJournal yesterdayJournal, final URLHTMLVisitJournal todayJournal,
+                                      final String emailAddress, final String fullName,
+                                      Mailer mailer){
+        return;
+    }
 }
