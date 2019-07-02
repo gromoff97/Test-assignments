aec7a12dcf92bbcbe3d7c7d4ba5b4c9e40e8dee7
gromoff97 <gromoff97@mail.ru>
2019-07-02 17:51:09 +0700

    name of maildata-class was changed

--- hashtable-url-html-task/src/main/java/home/grom/urljournaling/JournalsDifferenceMailData.java
+++ hashtable-url-html-task/src/main/java/home/grom/urljournaling/JournalsDifferenceMailData.java
@@ -1,10 +1,10 @@
 package home.grom.urljournaling;
 
-public final class MailData {
+public final class JournalsDifferenceMailData {
     private URLHTMLVisitJournal oldJournal;
     private URLHTMLVisitJournal freshJournal;
 
-    public MailData( URLHTMLVisitJournal oldJournal, URLHTMLVisitJournal freshJournal ) {
+    public JournalsDifferenceMailData( URLHTMLVisitJournal oldJournal, URLHTMLVisitJournal freshJournal ) {
         if ( null == oldJournal || null == freshJournal ) {
             throw new IllegalArgumentException("Non-null references of journals are required");
         }
