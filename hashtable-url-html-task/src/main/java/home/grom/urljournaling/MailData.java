0639e74d798c330a3a41bb53ccbdbab73fda9fe4
gromoff97 <gromoff97@mail.ru>
2019-07-02 17:37:04 +0700

    constructor for MailData was created and implemented

--- hashtable-url-html-task/src/main/java/home/grom/urljournaling/MailData.java
+++ hashtable-url-html-task/src/main/java/home/grom/urljournaling/MailData.java
@@ -1,4 +1,19 @@
 package home.grom.urljournaling;
 
 public final class MailData {
+    private URLHTMLVisitJournal oldJournal;
+    private URLHTMLVisitJournal freshJournal;
+
+    public MailData( URLHTMLVisitJournal oldJournal, URLHTMLVisitJournal freshJournal ) {
+        if ( null == oldJournal || null == freshJournal ) {
+            throw new IllegalArgumentException("Non-null references of journals are required");
+        }
+
+        if ( oldJournal == freshJournal ) {
+            throw new IllegalArgumentException("Different journals' instances are required");
+        }
+
+        this.oldJournal = oldJournal;
+        this.freshJournal = freshJournal;
+    }
 }
