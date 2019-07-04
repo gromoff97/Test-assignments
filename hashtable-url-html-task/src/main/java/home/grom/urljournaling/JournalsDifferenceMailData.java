93c51b7bd5f26ec54c83019b20d8da9828263008
gromoff97 <gromoff97@mail.ru>
2019-07-04 18:42:52 +0700

    getting difference of sets was implemented

--- hashtable-url-html-task/src/main/java/home/grom/urljournaling/JournalsDifferenceMailData.java
+++ hashtable-url-html-task/src/main/java/home/grom/urljournaling/JournalsDifferenceMailData.java
@@ -1,8 +1,14 @@
 package home.grom.urljournaling;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class JournalsDifferenceMailData {
     private URLHTMLVisitJournal oldJournal;
     private URLHTMLVisitJournal freshJournal;
+
+    private Set<String> uniqueOldUrls;
+    private Set<String> uniqueFreshUrls;
 
     public JournalsDifferenceMailData( URLHTMLVisitJournal oldJournal, URLHTMLVisitJournal freshJournal ) {
         if ( null == oldJournal || null == freshJournal ) {
@@ -15,5 +21,16 @@ public final class JournalsDifferenceMailData {
 
         this.oldJournal = oldJournal;
         this.freshJournal = freshJournal;
+
+        uniqueOldUrls = getDiffFrom(this.freshJournal.getVisitedURLSet(),this.oldJournal.getVisitedURLSet());
+        uniqueFreshUrls = getDiffFrom(this.oldJournal.getVisitedURLSet(),this.freshJournal.getVisitedURLSet());
+    }
+
+    private static Set<String> getDiffFrom( Set<String> a, Set<String> b ) {
+        Set<String> tmpSet = new HashSet<>(a);
+        if ( !b.isEmpty() ) {
+            tmpSet.removeAll(b);
+        }
+        return tmpSet;
     }
 }
