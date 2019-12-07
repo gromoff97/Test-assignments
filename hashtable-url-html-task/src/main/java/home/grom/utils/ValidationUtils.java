04e1073f2641a86e09453005f3349b0fd2f10c4e
gromoff97 <gromoff97@mail.ru>
2019-12-07 21:00:31 +0700

    Custom 'requireNonNull'-method throwing 'IllegalArgumentException' was implemented.

--- hashtable-url-html-task/src/main/java/home/grom/utils/ValidationUtils.java
+++ hashtable-url-html-task/src/main/java/home/grom/utils/ValidationUtils.java
@@ -1,3 +1,9 @@
 package home.grom.utils;
 
-public final class ValidationUtils { }
+public final class ValidationUtils {
+    public static <T> T requireNonNull(T obj, String exceptionMessage) {
+        if (obj == null)
+            throw new IllegalArgumentException(exceptionMessage);
+        return obj;
+    }
+}
