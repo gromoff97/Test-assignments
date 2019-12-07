33de04958b0c4865767e14bc983b3baa1cecdbbf
gromoff97 <gromoff97@mail.ru>
2019-12-07 21:11:12 +0700

    Constant variable representing default exception for blank String was created.

--- hashtable-url-html-task/src/main/java/home/grom/utils/ValidationUtils.java
+++ hashtable-url-html-task/src/main/java/home/grom/utils/ValidationUtils.java
@@ -3,6 +3,7 @@ package home.grom.utils;
 public final class ValidationUtils {
 
     private static final String NULL_OBJ_DEFAULT_MESSAGE = "Referencing to non-null instance is required.";
+    private static final String BLANK_STRING_DEFAULT_MESSAGE = "Non-blank String's instance is required.";
 
     public static <T> T requireNonNull(T obj, String exceptionMessage) {
         if (obj == null)
