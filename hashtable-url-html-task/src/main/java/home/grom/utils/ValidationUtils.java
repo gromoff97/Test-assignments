c306e367b1977c40fd56289297db34637f2f392e
gromoff97 <gromoff97@mail.ru>
2019-12-07 21:06:03 +0700

    Constant variable representing default exception for null-reference was created.

--- hashtable-url-html-task/src/main/java/home/grom/utils/ValidationUtils.java
+++ hashtable-url-html-task/src/main/java/home/grom/utils/ValidationUtils.java
@@ -1,6 +1,9 @@
 package home.grom.utils;
 
 public final class ValidationUtils {
+
+    private static final String NULL_OBJ_DEFAULT_MESSAGE = "Referencing to non-null instance is required.";
+
     public static <T> T requireNonNull(T obj, String exceptionMessage) {
         if (obj == null)
             throw new IllegalArgumentException(exceptionMessage);
